d379ed5713c41315b5247013ca81480e6d3aa8f6
mahdavis2024 <m.mahdavisefat@outlook.com>
2024-04-22 18:42:38 +0330

    second practice

---
+++ # Chapter 4 Try this 2.py
@@ -0,0 +1,50 @@
+# Chapter 4 Try this 2
+#TRY THIS: MANIPULATING STRINGS AND NUMBERS
+
+this_one = 'one'
+print (this_one)
+That = 'test this'
+print(That)
+#my-name = "my"
+my_name = 'my'
+print(my_name)
+your_name = "your"
+print(your_name)
+print(type(your_name))
+together = That + my_name 
+print(together)
+together = That +"   "+ my_name 
+print(together)
+#print(your_name - my_name)
+print(That*4)
+print((That +' ')*3)
+#print(my_name *2.5)
+
+
+#testing math and cmath
+from math import *
+q=5
+w=6
+e=7.2
+print(w*q)
+print(q*e)
+print(e+w)
+y=sqrt(w)
+u=pow(q,3)
+print(u)
+print(y)
+print(sin(30))
+r=1-9j
+t=2+8j
+print(r*t)
+from cmath import *
+print(w*q)
+y=sqrt(w)
+print(y)
+import math
+print(math.sqrt(w))
+print(sqrt(-1))
+print(pow(w,4))
+
+
+
