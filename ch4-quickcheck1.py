a550c513691f35c6e2c4cb1d54298b494815b8ea
mahdavis2024 <m.mahdavisefat@outlook.com>
2024-04-24 13:30:42 +0330

    ch4 quick check 1

---
+++ ch4-quickcheck1.py
@@ -0,0 +1,23 @@
+# Chapter 4 Quick check 1
+# QUICK CHECK: PYTHONIC STYLE
+''' Which of the following variable and function names do you think are not good Pythonic style?
+ Why?
+bar(),   varName,   VERYLONGVARNAME,   foobar,   longvarname,
+foo_bar(), really_very_long_var_name '''
+
+bar() 				 # good, functions should be all lowercase
+
+varName 			 # not good, variable names should be all lowercase
+varname 
+
+VERYLONGVARNAME			# not good, variable names should be all lowercase, underscores_for_readablitiy
+very_long_var_name
+
+foobar 				# good
+
+longvarname 		# not good, variable names should be all lowercase, underscores_for_readablitiy
+long_var_name
+
+foo_bar() 			# good, functions should be all lowercase, underscores_for_readablitiy
+
+really_very_long_var_name  # good, variable names should be all lowercase, underscores_for_readablitiy
