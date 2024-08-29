c65b4c2e151b8701190d715675a83ebdcd60d813
mahdavis2024 <m.mahdavisefat@outlook.com>
2024-08-29 18:28:33 +0330

    chapter 14 exercises

--- ch14-quickcheck3.py
+++ ch14-quickcheck3.py
@@ -7,4 +7,9 @@ B) Use one with statement for all file reads and another for all file writes.
 C) Use a with statement each time you read a file or write a file (for each line, for example).
 D) Use a with statement for each file that you read or write.'''
 
-D makes more sense. 
+# The best approach would be D) Use a with statement for each file that you read or write.
+# Using a with statement for each file ensures that each file is properly opened and closed, 
+# which is crucial for resource management. 
+# This approach helps prevent resource leaks 
+# and ensures that files are closed as soon as their operations are complete, 
+# even if an error occurs during the process
